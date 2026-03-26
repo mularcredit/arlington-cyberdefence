5699b947ac444bf5b57b6fa37be1053ce488fadb
mac <mac@macs-MacBook-Pro.local>
2026-03-26 07:54:55 +0300

    fix: compile all html templates to dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        requestQuotation: resolve(__dirname, 'request-quotation.html'),
+        quotation: resolve(__dirname, 'quotation.html')
+      }
+    }
+  }
 })
